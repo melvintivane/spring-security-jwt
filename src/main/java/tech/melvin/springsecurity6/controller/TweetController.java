1536c249f554ff651ab17ff1ffe0aa7ebb30ef37
Melvin Tivane <zen@localhost.localdomain>
2024-12-19 22:51:31 +0200

    refactor(controller): delegate tweet management logic to TweetService

--- src/main/java/tech/melvin/springsecurity6/controller/TweetController.java
+++ src/main/java/tech/melvin/springsecurity6/controller/TweetController.java
@@ -14,69 +14,39 @@ import tech.melvin.springsecurity6.entities.Role;
 import tech.melvin.springsecurity6.entities.Tweet;
 import tech.melvin.springsecurity6.repository.TweetRepository;
 import tech.melvin.springsecurity6.repository.UserRepository;
+import tech.melvin.springsecurity6.service.TweetService;
 
 import java.util.UUID;
 
 @RestController
 public class TweetController {
-    private final UserRepository userRepository;
-    private final TweetRepository tweetRepository;
+    private final TweetService tweetService;
 
-    public TweetController(UserRepository userRepository, TweetRepository tweetRepository) {
-        this.userRepository = userRepository;
-        this.tweetRepository = tweetRepository;
+    public TweetController(TweetService tweetService) {
+        this.tweetService = tweetService;
     }
 
     @GetMapping("/feed")
     public ResponseEntity<FeedDTO> getFeed(@RequestParam(value = "page", defaultValue = "0") int page,
                                            @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
 
-        var tweets = tweetRepository.findAll(
-                PageRequest.of(page, pageSize, Sort.Direction.DESC, "creationTimestamp"))
-                .map(tweet -> new FeedItemDTO(
-                        tweet.getTweetId(),
-                        tweet.getContent(),
-                        tweet.getUser().getUsername()
-                ));
-
-        return ResponseEntity.ok(new FeedDTO(
-                tweets.getContent(), page, pageSize, tweets.getTotalPages(), tweets.getTotalElements()));
+        FeedDTO feedDTO = tweetService.getFeed(page, pageSize);
+        return ResponseEntity.ok(feedDTO);
     }
 
     @PostMapping("/tweets")
     public ResponseEntity<Void> createTweet(@RequestBody CreateTweetDTO createTweetDTO,
                                             JwtAuthenticationToken token) {
-        var user = userRepository.findById(UUID.fromString(token.getName()))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found."));;
 
-        var tweet = new Tweet();
-        tweet.setUser(user);
-        tweet.setContent(createTweetDTO.content());
-
-        tweetRepository.save(tweet);
-
-        return ResponseEntity.ok().build();
+        tweetService.createTweet(createTweetDTO, token);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @DeleteMapping("/tweets/{id}")
     public ResponseEntity<Void> deleteTweet(@PathVariable("id") Long tweetId,
                                             JwtAuthenticationToken token) {
-        var user = userRepository.findById(UUID.fromString(token.getName()))
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
-        var tweet = tweetRepository.findById(tweetId)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-
-        var isAdmin = user.getRoles()
-                .stream()
-                .anyMatch(role -> role.getName().equalsIgnoreCase(Role.Values.ADMIN.name()));
-
-        if (isAdmin || tweet.getUser().getUserId().equals(UUID.fromString(token.getName()))) {
-            tweetRepository.deleteById(tweetId);
-        } else {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-        }
-
+        tweetService.deleteTweet(tweetId, token);
         return ResponseEntity.ok().build();
     }
 }
