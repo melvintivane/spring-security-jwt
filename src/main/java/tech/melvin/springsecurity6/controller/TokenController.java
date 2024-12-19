e82ae892a393e87b4778ff271564f05e9ebcf7f6
Melvin Tivane <zen@localhost.localdomain>
2024-12-19 22:50:35 +0200

    refactor(controller): delegate token management logic to TokenService

--- src/main/java/tech/melvin/springsecurity6/controller/TokenController.java
+++ src/main/java/tech/melvin/springsecurity6/controller/TokenController.java
@@ -1,61 +1,25 @@
 package tech.melvin.springsecurity6.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtClaimsSet;
-import org.springframework.security.oauth2.jwt.JwtEncoder;
-import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tech.melvin.springsecurity6.controller.dto.LoginRequest;
 import tech.melvin.springsecurity6.controller.dto.LoginResponse;
-import tech.melvin.springsecurity6.entities.Role;
-import tech.melvin.springsecurity6.repository.UserRepository;
+import tech.melvin.springsecurity6.service.TokenService;
 
-import java.time.Instant;
-import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
-    private final JwtEncoder jwtEncoder;
-    private final UserRepository userRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
 
-    public TokenController(JwtEncoder jwtEncoder, UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.jwtEncoder = jwtEncoder;
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    public TokenController(TokenService tokenService) {
+        this.tokenService = tokenService;
     }
-
 
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest) {
-        var user = userRepository.findByUsername(loginRequest.username());
-
-        if (user.isEmpty() || !user.get().isLoginCorrect(loginRequest, passwordEncoder)) {
-            throw new BadCredentialsException("User or Password is invalid!");
-        }
-
-        var now = Instant.now();
-        var expiresIn = 1000L;
-
-        var scopes = user.get().getRoles()
-                .stream()
-                .map(Role::getName)
-                .collect(Collectors.joining(" "));
-
-        var claims = JwtClaimsSet.builder()
-                .issuer("mybackend")
-                .subject(user.get().getUserId().toString())
-                .issuedAt(now)
-                .expiresAt(now.plusSeconds(expiresIn))
-                .claim("scope", scopes)
-                .build();
-
-        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
-
-        return ResponseEntity.ok(new LoginResponse(jwtValue, expiresIn));
+        LoginResponse loginResponse = tokenService.login(loginRequest);
+        return ResponseEntity.ok(loginResponse);
     }
 }
