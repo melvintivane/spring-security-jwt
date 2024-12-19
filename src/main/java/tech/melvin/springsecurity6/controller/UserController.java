82971ed0fbc8feda1237869c2febc973d875a547
Melvin Tivane <zen@localhost.localdomain>
2024-12-19 22:49:09 +0200

    refactor(controller): delegate user management logic to UserService

--- src/main/java/tech/melvin/springsecurity6/controller/UserController.java
+++ src/main/java/tech/melvin/springsecurity6/controller/UserController.java
@@ -1,61 +1,37 @@
 package tech.melvin.springsecurity6.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import tech.melvin.springsecurity6.controller.dto.CreateUserDTO;
-import tech.melvin.springsecurity6.entities.Role;
 import tech.melvin.springsecurity6.entities.User;
-import tech.melvin.springsecurity6.repository.RoleRepository;
-import tech.melvin.springsecurity6.repository.UserRepository;
+import tech.melvin.springsecurity6.service.UserService;
 
 import java.util.List;
-import java.util.Set;
 
 @RestController
 public class UserController {
-    private final UserRepository userRepository;
-    private final RoleRepository roleRepository;
-    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository, RoleRepository roleRepository, BCryptPasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.roleRepository = roleRepository;
-        this.passwordEncoder = passwordEncoder;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @Transactional
     @PostMapping("/users")
     public ResponseEntity<Void> register(@RequestBody CreateUserDTO userDTO) {
-        var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
-
-        var userFromDb = userRepository.findByUsername(userDTO.username());
-        if (userFromDb.isPresent()) {
-            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
-        }
-
-        var user = new User();
-        user.setUsername(userDTO.username());
-        user.setPassword(passwordEncoder.encode(userDTO.password()));
-        user.setRoles(Set.of(basicRole));
-
-        userRepository.save(user);
-
+        userService.save(userDTO);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/users")
     @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public ResponseEntity<List<User>> listUsers() {
-        var users = userRepository.findAll();
+        var users = userService.listUsers();
         return ResponseEntity.ok(users);
     }
 }
