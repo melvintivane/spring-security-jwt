6019372f7c73cfd1d81470b6eb5448f73e516473
Melvin Tivane <zen@localhost.localdomain>
2024-12-29 11:41:06 +0200

    refactor: correct import paths

--- src/main/java/tech/melvin/springsecurity6/controller/UserController.java
+++ src/main/java/tech/melvin/springsecurity6/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import tech.melvin.springsecurity6.controller.dto.CreateUserDTO;
-import tech.melvin.springsecurity6.entities.User;
+import tech.melvin.springsecurity6.entity.User;
 import tech.melvin.springsecurity6.service.UserService;
 
 import java.util.List;
